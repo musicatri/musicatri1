0a9133992805b8b4f5aacf0fe00d2a72f2638f44
musicatri <144524874+musicatri@users.noreply.github.com>
2023-10-22 16:08:07 -0700

    clean up

--- setup.sh
+++ setup.sh
@@ -1,23 +1,13 @@
-sudo apt install python3-pip
-sudo apt install ffmpeg
+sudo apt install python3-pip ffmpeg chromium chromium-driver
 python3 -m venv ./pyenv
 source ./pyenv/bin/activate
-pip install gtts
-pip install discord
-pip install requests
-pip install ast
-pip install asyncio
-pip install yt_dlp
-pip install selenium==4.8.2
-pip install bs4
-pip install pynacl
-pip install openai flask flask[async] waitress
+pip install gtts discord requests asyncio yt_dlp selenium==4.8.2 bs4 pynacl openai flask flask[async] waitress
 echo "[]" > waifulist.txt
 echo "{}" > plays.json
 echo "{}" > haogan.json
-echo "{}" > atrikey.json
 echo "{}" > blacklist.json
 echo "{}" > langpref.json
 mkdir songcache
 mkdir datacache
 mkdir ytdltemp
+python setup.py
