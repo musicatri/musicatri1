441698f268021f0640d72eeac53676c963ac11d8
musicatri <144524874+musicatri@users.noreply.github.com>
2023-10-31 23:47:30 -0700

    Update search163chrome.py

--- search163chrome.py
+++ search163chrome.py
@@ -107,7 +107,9 @@ def get_id_name_singer(id):
     return [music_id, music_name, music_singer]
     print(traceback.format_exc())
 def login163():
-    browser = webdriver.Chrome(executable_path=executable_path,desired_capabilities=caps)
+    chrome_options = Options()
+
+    browser = webdriver.Chrome(executable_path=executable_path, chrome_options=chrome_options,desired_capabilities=caps)
     url = "http://music.163.com/"
     browser.get(url=url)
     input("登录完成请按回车")
