5b7df2a11640278977509e10068525a5f8b3d4dd
musicatri <144524874+musicatri@users.noreply.github.com>
2023-11-15 01:48:30 -0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,15 @@
-sudo apt install python3 python3-pip ffmpeg nodejs npm
+sudo apt install python3 python3-pip ffmpeg
+sudo apt-get update
+sudo apt-get install -y ca-certificates curl gnupg
+sudo mkdir -p /etc/apt/keyrings
+curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
+NODE_MAJOR=20
+echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list
+sudo apt-get update
+sudo apt-get install nodejs  -y
 python3 -m venv ./pyenv
 source ./pyenv/bin/activate
-pip install gtts discord requests asyncio yt_dlp pynacl openai flask flask[async] waitress aiofile
+pip install gtts discord requests asyncio yt_dlp pynacl openai flask flask[async] waitress aiofiles
 echo "[]" > waifulist.txt
 echo "{}" > plays.json
 echo "{}" > haogan.json
@@ -14,4 +22,4 @@ git clone https://github.com/Binaryify/NeteaseCloudMusicApi.git
 cd NeteaseCloudMusicApi
 npm install
 cd ..
-python setup.py
+python3 setup.py
