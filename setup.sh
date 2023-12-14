18c251853c5fccc31c493894bdb2340087c72e9c
fbk1 <fbk1@localhost.localdomain>
2023-12-14 14:26:55 -0800

    clean setup

--- setup.sh
+++ setup.sh
@@ -1,12 +1,11 @@
-sudo apt install python3 python3-pip ffmpeg  python3-venv git -y
-sudo apt-get update
-sudo apt-get install -y ca-certificates curl gnupg
+sudo apt update
+sudo apt install python3 python3-pip ffmpeg  python3-venv git ca-certificates curl gnupg -y
 sudo mkdir -p /etc/apt/keyrings
 curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
 NODE_MAJOR=20
 echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list
-sudo apt-get update
-sudo apt-get install nodejs  -y
+sudo apt update
+sudo apt install nodejs  -y
 python3 -m venv pyenv
 ./pyenv/bin/python3 -m pip install gtts discord requests asyncio yt_dlp pynacl openai flask flask[async] waitress aiofiles flask_discord prettytable
 echo "[]" > waifulist.txt
